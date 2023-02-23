1d5385f6a7835cfd1214aa2a36b056e239920d4c
Kamil Khusnullin <comics01@mail.ru>
2023-02-23 21:53:57 +0300

    first homework

---
+++ baseinfosearch_1/src/__main__.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from crawler import Spider
+from utils import save_to_file
+
+
+async def main(memory):
+    filewrite = []
+    with open("index.txt", "w") as f:
+        i = 0
+        for url, content in memory.items():
+            i += 1
+            filewrite.append(asyncio.ensure_future(save_to_file(content, f"save_pages/{i}.html")))
+            f.write(f"{i}.html - {url}\n")
+    await asyncio.gather(*filewrite)
+
+
+urls = []
+with open("urls.txt", "r") as f:
+    for line in f:
+        urls.append(line)
+
+spider = Spider(urls)
+
+spider.run()
+
+asyncio.run(main(spider.memory))
---
+++ baseinfosearch_1/src/auto_filling_urls.py
@@ -0,0 +1,27 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    url = 'https://en.wikipedia.org/wiki/'
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+
+    urls = []
+    for link in soup.find_all('a'):
+        urls.append(link.get('href'))
+
+    with open("urls.txt", "w") as f:
+        for url in urls:
+            f.write(f"{url}\n")
+            f.write(f"https://en.wikipedia.org/wiki/\n")
+
+
+set_ = set()
+with open("urls.txt", "r") as f:
+    for line in f:
+        set_.add(line)
+
+with open("urls.txt", "w") as f:
+    for line in set_:
+        f.write(line)
